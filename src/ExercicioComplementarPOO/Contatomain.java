f42798e21435889815adc95b0563e19ca8a2aa4b
renanluizturra <266590544+renanluizturra@users.noreply.github.com>
2026-03-16 10:50:40 -0300

    Update Contatomain.java

--- src/ExercicioComplementarPOO/Contatomain.java
+++ src/ExercicioComplementarPOO/Contatomain.java
@@ -1,10 +1,62 @@
 package ExercicioComplementarPOO;
 
+import java.util.Scanner;
+
 public class Contatomain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
 
-}
+		        Scanner sc = new Scanner(System.in);
+		        Contato agenda = new Contato();
+
+		        int opcao;
+
+		        do {
+
+		            System.out.println("\n=== AGENDA DE CONTATOS ===");
+		            System.out.println("1 - Adicionar contato");
+		            System.out.println("2 - Listar contatos");
+		            System.out.println("0 - Sair");
+		            System.out.print("Escolha: ");
+
+		            opcao = sc.nextInt();
+		            sc.nextLine();
+
+		            switch (opcao) {
+
+		                case 1:
+
+		                    System.out.print("Nome: ");
+		                    String nome = sc.nextLine();
+
+		                    System.out.print("Telefone: ");
+		                    String telefone = sc.nextLine();
+
+		                    agenda.adicionar(nome, telefone);
+
+		                    break;
+
+		                case 2:
+
+		                    agenda.listar();
+
+		                    break;
+
+		                case 0:
+
+		                    System.out.println("Saindo da agenda...");
+
+		                    break;
+
+		                default:
+
+		                    System.out.println("Opção inválida!");
+
+		            }
+
+		        } while (opcao != 0);
+
+		        sc.close();
+		    }
+		}
